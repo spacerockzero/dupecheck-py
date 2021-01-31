7c13f218c03c82f66baff506ea2ecdac034ebe63
= <janderson@familysearch.org>
2021-01-30 19:36:54 -0700

    revved version since we added sliding window

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="copycheck",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     description="Check for duplicate word series between generated text and a dataset",
     author="Jakob Anderson",
     license="MIT",
